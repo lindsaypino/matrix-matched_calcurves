4cb7941b76d8a6fe4357c2ffe4fa8813728f52b8
LPINO-PC\lpino <lpino@uw.edu>
2019-06-10 13:58:41 -0700

    incorporating wout's code review suggestions

--- bin/calibration_curve_fitting_dataanalysis.py
+++ bin/calibration_curve_fitting_dataanalysis.py
@@ -5,7 +5,6 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
 from tqdm import tqdm
 import argparse
 import random
@@ -22,25 +21,25 @@ def read_input(filename, col_conc_map_file):
 
     # if numFragments is a column, it's an Encyclopedia file
     if 'numFragments' in header_line:
-        sys.stdout.write("Input identified as EncyclopeDIA *.elib.peptides.txt filetype.\n")
+        sys.stdout.write('Input identified as EncyclopeDIA *.elib.peptides.txt filetype.\n')
 
-        df = pd.read_table(filename, sep=None, engine="python")  # read in the table
+        df = pd.read_csv(filename, sep=None, engine='python')
         df = df.drop(['numFragments', 'Protein'], 1)  # make a quantitative df with just curve points and peptides
-        col_conc_map = pd.read_csv(col_conc_map_file, sep=',', engine="python")
+        col_conc_map = pd.read_csv(col_conc_map_file)
         df = df.rename(columns=col_conc_map.set_index('filename')['concentration'])  # map filenames to concentrations
-        df = df.rename(columns={'Peptide': 'peptide'})  # rename the peptide column
-        df_melted = pd.melt(df, id_vars=['peptide'])  # melt the dataframe down
+        df = df.rename(columns={'Peptide': 'peptide'})
+        df_melted = pd.melt(df, id_vars=['peptide'])
         df_melted.columns = ['peptide', 'curvepoint', 'area']
         df_melted = df_melted[df_melted['curvepoint'].isin(col_conc_map['concentration'])]
 
     # require columns for File Name, Total Area Fragment, Peptide Sequence
     # TODO: option for Total Area Ratio?
-    elif 'Total Area Fragment' in header_line and 'Peptide Sequence' in header_line and 'File Name' in header_line:
-        sys.stdout.write("Input identified as Skyline export filetype. \n")
+    elif all(col in header_line for col in ['Total Area Fragment', 'Peptide Sequence', 'File Name']):
+        sys.stdout.write('Input identified as Skyline export filetype. \n')
 
-        df_melted = pd.read_csv(filename, sep=None, engine="python")  # read in the csv
+        df_melted = pd.read_csv(filename)
         df_melted.rename(columns={'File Name': 'filename'}, inplace=True)
-        col_conc_map = pd.read_csv(col_conc_map_file, sep=',', engine="python")
+        col_conc_map = pd.read_csv(col_conc_map_file)
 
         # remove any data for which there isn't a map key
         df_melted = df_melted[df_melted['filename'].isin(col_conc_map['filename'])]
@@ -49,16 +48,15 @@ def read_input(filename, col_conc_map_file):
         df_melted = pd.merge(df_melted, col_conc_map, on='filename', how='outer')
 
         # clean up column names to match downstream convention
-        df_melted.rename(columns={'Total Area Fragment': 'area'}, inplace=True)
-        df_melted.rename(columns={'Peptide Sequence': 'peptide'}, inplace=True)
-        df_melted.rename(columns={'concentration': 'curvepoint'}, inplace=True)
+        df_melted.rename(columns={'Total Area Fragment': 'area',
+                                  'Peptide Sequence': 'peptide',
+                                  'concentration': 'curvepoint'}, inplace=True)
 
         # remove points that didn't have a mapping (NA)
         df_melted['curvepoint'].replace('', np.nan, inplace=True)
         df_melted.dropna(subset=['curvepoint'], inplace=True)
 
         df_melted['area'].fillna(0, inplace=True)  # replace NA with 0
-
 
     # convert the curve points to numbers so that they sort correctly
     df_melted['curvepoint'] = pd.to_numeric(df_melted['curvepoint'])
@@ -68,10 +66,10 @@ def read_input(filename, col_conc_map_file):
 
 # associates a multiplier value to the curvepoint a la single-point calibration
 def associate_multiplier(df, multiplier_file):
-    mutliplier_df = pd.read_csv(multiplier_file, sep=None, engine="python")  # read in the multiplier file
+    mutliplier_df = pd.read_csv(multiplier_file)
 
     # merge the multiplier with the data frame
-    merged_df = pd.merge(df, mutliplier_df, on="peptide", how="inner")
+    merged_df = pd.merge(df, mutliplier_df, on='peptide', how='inner')
     merged_df['curvepoint_multiplied'] = merged_df['curvepoint'] * merged_df['multiplier']
     multiplied_df = merged_df[['peptide', 'curvepoint_multiplied', 'area']]
     multiplied_df.columns = ['peptide', 'curvepoint', 'area']
@@ -87,7 +85,7 @@ def fit_by_lmfit_yang(x, y):
         b = params['b'].value
         c = params['c'].value
         model = np.maximum(c, a*x+b)
-        return (model - data) * weight
+        return (model-data) * weight
 
     # parameter initialization
     def initialize_params(x, y):
@@ -102,11 +100,11 @@ def fit_by_lmfit_yang(x, y):
         bottom_point = min(conc_list)
 
         # using the means, calculate a slope (y1-y2/x1-x2)
-        linear_slope = (mean_y[second_top] - mean_y[top_point]) / (second_top - top_point)
+        linear_slope = (mean_y[second_top]-mean_y[top_point]) / (second_top-top_point)
         # find the noise intercept using average of bottom three points
         noise_intercept = mean_y[bottom_point]
         # find the linear intercept using linear slope (b = y-mx) and the top point
-        linear_intercept = mean_y[top_point] - (linear_slope * top_point)
+        linear_intercept = mean_y[top_point] - (linear_slope*top_point)
 
         # edge case catch?
         if noise_intercept < linear_intercept:
@@ -117,17 +115,14 @@ def fit_by_lmfit_yang(x, y):
     params = Parameters()
     initial_a, initial_b, initial_c = initialize_params(x,y)
     initial_cminusb = initial_c - initial_b
-    params.add('a', value=initial_a, min=0.0, vary=True)  # slope linear
-    params.add('b', value=initial_b, vary=True)  # intercept linear
+    params.add('a', value=initial_a, min=0.0, vary=True)  # slope signal
+    params.add('b', value=initial_b, vary=True)  # intercept signal
     params.add('c_minus_b', value=initial_cminusb, min=0.0, vary=True)
     params.add('c', expr='b + c_minus_b')
 
-    weights = np.minimum(1/(np.asarray(np.sqrt(x), dtype=float)+np.finfo(np.float).eps), 1000)  # inverse weights
+    weights = np.minimum(1 / (np.asarray(np.sqrt(x), dtype=float)+np.finfo(np.float).eps), 1000)  # inverse weights
     minner = Minimizer(fcn2min, params, fcn_args=(x, y, weights))
     result = minner.minimize()
-
-    #final = y + result.residual
-    #print report_fit(result)
 
     return result, minner
 
@@ -138,13 +133,13 @@ def calculate_lod(model_params, df, std_mult):
     m_noise, b_noise, m_linear, b_linear = model_params
 
     # calculate the standard deviation for the noise segment
-    intersection = (b_linear - b_noise) / (m_noise - m_linear)
+    intersection = (b_linear-b_noise) / (m_noise-m_linear)
     std_noise = np.std(df['area'].loc[(df['curvepoint'].astype(float) < intersection)])
 
     if m_linear < 0:  # catch edge cases where there is only noise in the curve
         LOD = float('Inf')
     else:
-        LOD = (b_noise + (std_mult*std_noise) - b_linear)/m_linear
+        LOD = (b_noise + (std_mult*std_noise) - b_linear) / m_linear
     lod_results = [LOD, std_noise]
 
     # LOD edge cases
@@ -170,26 +165,24 @@ def calculate_loq(model_params, boot_results, cv_thresh=0.2):
         LOQ = float('Inf')
     else:
         # subset the bootstrap results for just those values above the LOD
-        boot_subset = boot_results[(boot_results['boot_x'] > LOD) & (boot_results['boot_cv'] <= cv_thresh)]
+        boot_subset = boot_results[(boot_results['boot_x']>LOD) & (boot_results['boot_cv']< cv_thresh)]
 
-        if len(boot_subset) == 0:
+        if boot_subset.empty:
             LOQ = float('Inf')
         else:
-            LOQ = min(boot_subset['boot_x'])
+            LOQ = boot_subset['boot_x'].min()
 
-        # LOQ edge cases
-        if LOQ >= float(max(set(list(boot_results['boot_x'])))):
-            LOQ = float('Inf')
-        elif LOQ <= 0:  # no idea why this should ever happen but just in case
-            LOQ = float('Inf')
+            # LOQ edge cases
+            if LOQ >= boot_results['boot_x'].max() or LOQ <= 0:
+                LOQ = float('Inf')
 
     return LOQ
 
 
 # determine prediction interval by bootstrapping
-def bootstrap_many(df, new_x, bootreps=100):
+def bootstrap_many(df, new_x, num_bootreps=100):
 
-    def bootstrap_once(df, new_x, iter):
+    def bootstrap_once(df, new_x, iter_num):
 
         resampled_df = df.sample(n=len(df), replace=True)
         boot_x = np.array(resampled_df['curvepoint'], dtype=float)
@@ -198,12 +191,11 @@ def bootstrap_many(df, new_x, bootreps=100):
         new_intersection = float('Inf')
 
         if fit_result.params['a'].value > 0:
-            new_intersection = (fit_result.params['b'].value - fit_result.params['c'].value) / (0. - fit_result.params['a'].value)
+            new_intersection = (fit_result.params['b'].value - fit_result.params['c'].value) /\
+                               (0. - fit_result.params['a'].value)
 
             # consider some special edge cases
-            if new_intersection > max(boot_x):
-                new_intersection = float('Inf')
-            elif new_intersection < 0.:
+            if new_intersection > max(boot_x) or new_intersection < 0.:
                 new_intersection = float('Inf')
 
         yresults = []
@@ -213,28 +205,20 @@ def bootstrap_many(df, new_x, bootreps=100):
             elif i <= new_intersection:  # if the new_x is in the noise,
                 pred_y = fit_result.params['c'].value
             elif i > new_intersection:
-                pred_y = (fit_result.params['a'].value * i) + fit_result.params['b'].value
+                pred_y = (fit_result.params['a'].value*i) + fit_result.params['b'].value
             yresults.append(pred_y)
-        iter_results = pd.DataFrame(data={'boot_x': new_x, iter: yresults})
+        iter_results = pd.DataFrame(data={'boot_x': new_x, iter_num: yresults})
 
         return iter_results
 
-    if not df.size or np.isnan(new_x).any():
-        boot_summary = pd.DataFrame({'boot_x': [],
-                                    'count':[],
-                                    'mean':[],
-                                    'std':[],
-                                    'min':[],
-                                    '5%':[],
-                                    '50%':[],
-                                    '95%':[],
-                                    'max':[],
-                                    'boot_cv':[]})
+    if df.empty or np.isnan(new_x).any():
+        boot_summary = pd.DataFrame(columns=['boot_x', 'count', 'mean', 'std', 'min',
+                                             '5%', '50%', '95%', 'max', 'boot_cv'])
 
     else:
         # Bootstrap the data (e.g. resample the data with replacement), eval prediction (new_y) at each new_x
         boot_results = pd.DataFrame(data={'boot_x': new_x})
-        for i in range(bootreps):
+        for i in range(num_bootreps):
             iteration_results = bootstrap_once(df, new_x, i)
 
             boot_results = pd.merge(boot_results, iteration_results, on='boot_x')
@@ -250,10 +234,6 @@ def bootstrap_many(df, new_x, bootreps=100):
 
         # calculate the bootstrapped CV
         boot_summary['boot_cv'] = boot_summary['std']/boot_summary['mean']
-
-    boot_summary.to_csv(path_or_buf=os.path.join(output_dir,
-                                                 'bootstrapsummary_'+str(list(set(df['peptide']))[0])+'.csv'),
-                        index=True)
 
     return boot_summary
 
@@ -295,9 +275,10 @@ def build_plots(x, y, model_results, boot_results, std_mult):
     ### top plot: linear scale x axis
     plt.subplot(2, 1, 1)
     plt.plot(x, y, 'o')  # scatterplot of the data
-    plt.fill_between(boot_results['boot_x'],
-                     boot_results['mean']-boot_results['std'], boot_results['mean']+boot_results['std'],
-                     color='y', alpha=0.3)
+    if not boot_results.empty:
+        plt.fill_between(boot_results['boot_x'],
+                         boot_results['mean']-boot_results['std'], boot_results['mean']+boot_results['std'],
+                         color='y', alpha=0.3)
     add_line_to_plot(slope_noise, intercept_noise, 'linear', '-', 'g')  # add noise segment line
     add_line_to_plot(slope_noise, (intercept_noise + (std_mult*std_noise)), 'linear', '--', setcolor='0.5')
     if slope_linear > 0:  # add linear segment line
@@ -311,7 +292,7 @@ def build_plots(x, y, model_results, boot_results, std_mult):
                 color='c',
                 label=('LOQ = %.3e' % LOQ))
 
-    plt.ylabel("signal")
+    plt.ylabel('signal')
 
     # force axis ticks to be scientific notation so the plot is prettier
     plt.ticklabel_format(style='sci', axis='both', scilimits=(0, 0))
@@ -337,8 +318,8 @@ def build_plots(x, y, model_results, boot_results, std_mult):
     plt.axhline(y=0.20, color='r', linestyle='dashed')
 
     #plt.title(peptide, y=1.08)
-    plt.xlabel("quantity")
-    plt.ylabel("CV")
+    plt.xlabel('quantity')
+    plt.ylabel('CV')
 
     # force axis ticks to be scientific notation so the plot is prettier
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
@@ -358,12 +339,11 @@ def build_plots(x, y, model_results, boot_results, std_mult):
     plt.close()
 
 
-
 # usage statement and input descriptions
 parser = argparse.ArgumentParser(
-    description="A  model for fitting calibration curve data. Takes calibration curve measurements as input, and \
+    description='A  model for fitting calibration curve data. Takes calibration curve measurements as input, and \
                 returns the Limit of Detection (LOD) and Limit of Quantitation (LOQ) for each peptide measured in \
-                the calibration curve.",
+                the calibration curve.',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('curve_data', type=str,
@@ -372,9 +352,9 @@ parser.add_argument('curve_data', type=str,
 parser.add_argument('filename_concentration_map', type=str,
                     help='a comma-delimited file containing maps between filenames and the concentration point \
                             they represent (two columns named "filename" and "concentration")')
-parser.add_argument('--std_mult', default=3, type=float,
+parser.add_argument('--std_mult', default=2, type=float,
                     help='specify a multiplier of the standard deviation of the noise for determining limit of \
-                    detection (LOD) (Note: this must be a value greater than or equal to 3)')
+                    detection (LOD)')
 parser.add_argument('--cv_thresh', default=0.2, type=float,
                     help='specify a coefficient of variation threshold for determining limit of quantitation (LOQ) \
                             (Note: this should be a decimal, not a percentage, e.g. 20%CV threshold should be input as \
@@ -388,6 +368,8 @@ parser.add_argument('--output_path', default=os.getcwd(), type=str,
                     help='specify an output path for figures of merit and plots')
 parser.add_argument('--plot', default='y', type=str,
                     help='yes/no (y/n) to create individual calibration curve plots for each peptide')
+parser.add_argument('--verbose', default='n', type=str,
+                    help='output a detailed summary of the bootstrapping step')
 
 # parse arguments from command line
 args = parser.parse_args()
@@ -395,9 +377,11 @@ raw_file = args.curve_data
 col_conc_map_file = args.filename_concentration_map
 cv_thresh = args.cv_thresh
 std_mult = args.std_mult
+bootreps = args.bootreps
 multiplier_file = args.multiplier_file
 output_dir = args.output_path
 plot_or_not = args.plot
+verbose = args.verbose
 
 # read in the data
 quant_df_melted = read_input(raw_file, col_conc_map_file)
@@ -445,8 +429,13 @@ for peptide in tqdm(quant_df_melted['peptide'].unique()):
     # calculate coefficients of variation for discrete bins over the linear range (default bins=100)
     x_i = np.linspace(LOD, max(x), num=100, dtype=float)
 
-    # bootreps: number of times to do the bootstrapping for each new x-point (default 100)
-    bootstrap_df = bootstrap_many(subset, new_x=x_i, bootreps=100)
+    bootstrap_df = bootstrap_many(subset, new_x=x_i, num_bootreps=bootreps)
+
+    if verbose == 'y':
+        boot_summary.to_csv(path_or_buf=os.path.join(output_dir,
+                                                     'bootstrapsummary_' + str(list(set(df['peptide']))[0]) + '.csv'),
+                            index=True)
+
     LOQ = calculate_loq(model_parameters, bootstrap_df, cv_thresh)
     model_parameters = np.append(model_parameters, LOQ)
 
@@ -461,5 +450,5 @@ for peptide in tqdm(quant_df_melted['peptide'].unique()):
                                                   'stndev_noise'])
     peptide_fom = peptide_fom.append(new_df_row)
 
-    peptide_fom.to_csv(path_or_buf=os.path.join(output_dir, 'figuresofmerit.csv'),
-                       index=False)
+peptide_fom.to_csv(path_or_buf=os.path.join(output_dir, 'figuresofmerit.csv'),
+                   index=False)
