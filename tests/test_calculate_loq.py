04830af171812662cc392bfa1ca80c2c3d580fd1
Seth Just <sethjust@sethjust.com>
2022-05-18 15:44:44 -0700

    Implement dead-simple subprocess benchmark.

--- tests/test_calculate_loq.py
+++ tests/test_calculate_loq.py
@@ -2,9 +2,16 @@
 `benchmarks.py` -- run benchmarks on `calculate_loq.py`
 """
 
+import subprocess
+
 
 def bench_calculate_loq(tmp_path):
-    raise RuntimeError("TODO")
+    cmd = [
+        "python", "bin/calculate-loq.py",
+        "--output_path", tmp_path,
+        "data/one_protein.csv", "data/filename2samplegroup_map.csv"
+    ]
+    subprocess.run(cmd, check=True)
 
 
 def test_bench_calculate_loq(benchmark, tmp_path):
